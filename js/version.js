73b26c209cb692675041167a4dd938fd85c5bd76
kevinagustiana8-gif <kevinagustiana8@gmail.com>
2026-09-25 08:23:51 +0700

    Update version.js

--- js/version.js
+++ js/version.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 window.APP_VERSION = {
-  version : '2.5.5',
+  version : '3.0.0',
   build   : '2026-09-25',
   name    : 'Expiry RTC',
   author  : 'Kevin Agustiana'
